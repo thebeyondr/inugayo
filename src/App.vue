748ff43ad82d47716bc273d226422175e39419b4
ruffsamurai <jeopatto@gmail.com>
2017-07-26 22:39:46 -0500

    Added table + missing dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,69 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Inugayo: A Canine Sanctuary</h1>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel panel-heading">
+        Our dogs
+      </div> 
+      <div class="panel-body">
+        <div class="table table-striped">
+          <table>
+            <thead>
+             <tr>
+               <th>Name</th>
+               <th>Age</th>
+               <th>Gender</th>
+             </tr>
+            </thead>
+            <tbody>
+              <tr v-for ="dog in dogs">
+                <td>{{dog.name}}</td>
+                <td>{{dog.age}}</td>
+                <td>{{dog.gender}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-import Firebase from 'firebase'
+    import Hello from './components/Hello'
+    import Firebase from 'firebase'
 
-let config ={
-    apiKey: "AIzaSyByovdvO1h40MXJQVADprxqi0uMHNOm3EQ",
-    authDomain: "inugayo-56a7f.firebaseapp.com",
-    databaseURL: "https://inugayo-56a7f.firebaseio.com",
-    projectId: "inugayo-56a7f",
-    storageBucket: "inugayo-56a7f.appspot.com",
-    messagingSenderId: "73025774043"
-}
+    let config = {
+        apiKey: "AIzaSyByovdvO1h40MXJQVADprxqi0uMHNOm3EQ",
+        authDomain: "inugayo-56a7f.firebaseapp.com",
+        databaseURL: "https://inugayo-56a7f.firebaseio.com",
+        projectId: "inugayo-56a7f",
+        storageBucket: "inugayo-56a7f.appspot.com",
+        messagingSenderId: "73025774043"
+    }
 
-let app = Firebase.initializeApp(config);
-let db = app.database();
+    let app = Firebase.initializeApp(config);
+    let db = app.database();
 
-let dogsRef= db.ref('dogs');
+    let dogsRef = db.ref('dogs');
 
-export default {
-  name: 'app',
-  firebase: {
-    dogs: dogsRef
-  }
-}
+    export default {
+        name: 'app',
+        firebase: {
+            dogs: dogsRef
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
